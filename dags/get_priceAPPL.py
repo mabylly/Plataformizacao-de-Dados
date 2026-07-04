c9ef3f2cb2bb9d2e6e2a2a79e813d0bc2ac855dd
mabylly <mabyllyneres1@gmail.com>
2026-07-04 15:39:16 -0300

    Adicionando card 8

---
+++ dags/get_priceAPPL.py
@@ -0,0 +1,23 @@
+from aiflow import DAG
+from airflow.decorators import task
+from datetime import datetime
+
+with DAG(
+    "get_price_APPL", 
+    start_date=datetime(2026, 1, 1), 
+    schedule_interval="@daily", 
+    catchup=False) as dag:
+
+    @task
+    def extract(stock):
+        return stock
+
+    @task
+    def process(stock):
+        return stock
+
+    @task
+    def send_email(stock):
+        return stock
+
+    send_email(process(extract(126)))
